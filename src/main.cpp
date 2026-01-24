7495e8a30e821086e68c40ff657950c3f87909aa
Marios Dallas <159694805+CaettoGattowski@users.noreply.github.com>
2026-01-24 17:41:56 +0100

    Fixed VSC IDE integration :D

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,24 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
 #include <stdexcept>
+#include <vector>
 #include <iostream>
 
-int main() {
-    if (!glfwInit())
-        throw std::runtime_error("Failed to initialize GLFW");
 
+GLFWwindow * window;
+
+void initWIndow(std::string wName = "Test Window", const int width = 800, const int height = 600)
+{
+    // initialise GLFW
+    glfwInit();
+
+    // set GLFW to NOT work with OpenGL
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-    glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
-    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+}
+int main() 
+{
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Smoke Test", nullptr, nullptr);
-    if (!window) {
-        glfwTerminate();
-        throw std::runtime_error("Failed to create GLFW window");
-    }
-
-    glfwShowWindow(window);
-
-    // Vulkan instance (minimal)
-    VkInstance instance;
-    VkApplicationInfo appInfo{};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "SmokeTest";
-    appInfo.apiVersion = VK_API_VERSION_1_3;
-
-    uint32_t extCount = 0;
-    const char** exts = glfwGetRequiredInstanceExtensions(&extCount);
-
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
-    createInfo.enabledExtensionCount = extCount;
-    createInfo.ppEnabledExtensionNames = exts;
-
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
-        throw std::runtime_error("Failed to create Vulkan instance");
-
-    std::cout << "Vulkan instance created!\n";
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();  // process window events
-    }
-
-    vkDestroyInstance(instance, nullptr);
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
+    
     return 0;
 }
